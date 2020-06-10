f84223f259badb8568891bd67c753fb10ad9f471
Kircho Sharkovski <kircoosarkovski@gmail.com>
2020-06-10 15:06:29 +0200

    Radovis_tour starter

---
+++ android/app/src/main/kotlin/com/example/radovis_tour/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.radovis_tour
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          textTheme: GoogleFonts.openSansTextTheme(
+        Theme.of(context).textTheme,
+      )),
+      home: HomePageApp(),
+    );
+  }
+}
+
+class HomePageApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Center(
+            child: Text('Radovis'),
+          ),
+        ),
+      ),
+    );
+  }
+}
